0bf447fc80e5d6e78e016985a1f85e313d5a41ab
unknown <jeliang4@697-334-Waymoot.sf.k12.internal>
2017-01-30 15:30:43 -0800

    things

---
+++ lua2/main.lua
@@ -0,0 +1,7 @@
+display.newImage("aquariumbackgroundIPhone.jpg", 160, 160)
+
+local things = function()
+	print("hello")
+end
+
+things()
---
+++ main.lua
@@ -0,0 +1,5 @@
+print("hello")
+
+local text = display.newText("hello", 120, 120, "Arial", 40)
+text:setFillColor(.12,.32,.12)
+local pix = display.newRect(12, 21, 12, 54)
