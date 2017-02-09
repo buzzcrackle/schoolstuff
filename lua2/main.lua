1e9b3f2d5ae02a0d9ba440273a1dc3d10bf99f97
unknown <jeliang4@697-334-Waymoot.sf.k12.internal>
2017-02-09 15:25:25 -0800

    idik

--- lua2/main.lua
+++ lua2/main.lua
@@ -1,28 +1,52 @@
-local box = newRect(160, 0, 100, 100)
+physics = require("physics")
 
-local scale = function(img, scale, delay)
+physics.start()
+
+local buildings = {}
+
+for i=1, 25 do
+
+	local q = math.random(0, 320)
+	local w = 520
+	local e = math.random(10, 25)
+	local r = math.random(30, 80)
+
+	buildings[i] = display.newRect(q, w, e, r)
+	physics.addBody(buildings[i], "static")
+end
+
+local box = display.newRect(0, 240, 0, 480)
+local box2 = display.newRect(320, 240, 0, 480)
+local ground = display.newRect(160, 530, 320, 0)
+local boomboom = display.newImage("boomboom.png", 160, 0)
+	--boomboom:scale(0.01, 0.01)
+	boomboom.rotation = 180
+	--ground:scale(0.5, 0.5)
+
+physics.addBody(boomboom, {density=0.5, friction=0.5, bounce=0.0000001})
+physics.addBody(box, "static")
+physics.addBody(box2, "static")
+physics.addBody(ground, "static")
+
+function saem(img, scale, delay)
 	transition.scaleTo(img, { xScale = scale, yScale = scale, time = delay})
 end
 
-local rotate = function(img, angle, delay)
-	transition.to(img, {rotation = img.rotation-angle, time = delay})
+function boom()
+	x = math.random(0, 320)
+	y = math.random(0, 480)
+	z = math.random(10, 25)
+	local c1 = display.newCircle(x,y, 10)
+	local c2 = display.newCircle(x,y, 9)
+	c2:setFillColor(0.9,0.4,0)
+	saem(c1, z, 800)
+	saem(c2, z, 800)
 end
 
---To Move Objects
-local moveTo = function(img, x2, y2, delay)
-	transition.moveTo(img, {x = x2, y = y2, time = delay})
+function apocalypse()
+	for i=0, 50 do
+		timer.performWithDelay(math.random(0, 10000), boom)
+	end
 end
 
-local plus = function(x, y)
-	local hor = display.newRect(x, y, 100, 20)
-	local vert = display.newRect(x, y, 20, 100)
-end
-
-local equal = function(x, y)
-	local top = display.newRect(x, y, 100, 20)
-	local bottom = display.newRect(x, y + 40, 100, 20)
-end
-
-for i=1, 1000, 1 do
-	transition.moveTo(box, {x = 160, y = , time = delay})
-end
+timer.performWithDelay(5000, apocalypse)
