b46ad53d80e1fed7fc7470daf748806add3f1e26
unknown <jeliang4@697-334-Waymoot.sf.k12.internal>
2017-02-13 15:30:27 -0800

    things

--- lua2/main.lua
+++ lua2/main.lua
@@ -2,7 +2,7 @@ physics = require("physics")
 
 physics.start()
 
-local buildings = {}
+local building = {}
 
 for i=1, 25 do
 
@@ -10,42 +10,46 @@ for i=1, 25 do
 	local w = 520
 	local e = math.random(10, 25)
 	local r = math.random(30, 80)
-
-	buildings[i] = display.newRect(q, w, e, r)
-	physics.addBody(buildings[i], "static")
 end
 
+local build = display.newRect(160, 400, 100, 247.5)
 local box = display.newRect(0, 240, 0, 480)
 local box2 = display.newRect(320, 240, 0, 480)
 local ground = display.newRect(160, 530, 320, 0)
-local boomboom = display.newImage("boomboom.png", 160, 0)
+local boomboom = display.newImage("boomboom.png", 160, -100)
 	--boomboom:scale(0.01, 0.01)
 	boomboom.rotation = 180
-	--ground:scale(0.5, 0.5)
 
 physics.addBody(boomboom, {density=0.5, friction=0.5, bounce=0.0000001})
 physics.addBody(box, "static")
 physics.addBody(box2, "static")
 physics.addBody(ground, "static")
+physics.addBody(build, "static")
 
 function saem(img, scale, delay)
 	transition.scaleTo(img, { xScale = scale, yScale = scale, time = delay})
 end
 
-function boom()
-	x = math.random(0, 320)
-	y = math.random(0, 480)
-	z = math.random(10, 25)
-	local c1 = display.newCircle(x,y, 10)
-	local c2 = display.newCircle(x,y, 9)
-	c2:setFillColor(0.9,0.4,0)
-	saem(c1, z, 800)
-	saem(c2, z, 800)
+function boom(v1, v2)
+	display.remove(boomboom)
+	transition.fadeOut(v1, {time=4000})
+	transition.fadeOut(v2, {time=4000})
+	for i=1, table.maxn(building) do
+		
+	end
 end
 
 function apocalypse()
-	for i=0, 50 do
-		timer.performWithDelay(math.random(0, 10000), boom)
+	local x, y = boomboom:localToContent(0,0)
+
+	local c1 = display.newCircle(x,y, 10)
+	local c2 = display.newCircle(x,y, 9)
+		c2:setFillColor(0.9,0.4,0)
+	saem(c1, 100, 800)
+	saem(c2, 100, 800)
+
+	for i=1, 50 do
+		timer.performWithDelay(math.random(0, 10000), boom(c1, c2))
 	end
 end
 
